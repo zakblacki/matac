8172d8c77605cc11dda13abf5370b12a6c11f0b4
joesdevil <139871156+joesdevil@users.noreply.github.com>
2023-09-10 18:50:12 +0200

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -7,7 +7,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = os.getenv('DEBUG','True') == 'True'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =os.getenv('SECRET_KEY', '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj') 
-ALLOWED_HOSTS =["matacor.com","www.matacor.com","104.248.143.27"]
+ALLOWED_HOSTS =["matacor.com","www.matacor.com","207.154.249.107"]
  
 INSTALLED_APPS = [
     'django.contrib.admin',
